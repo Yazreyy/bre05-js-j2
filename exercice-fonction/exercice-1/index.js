0a824e31fc96db96befd79fc15492781755748eb
Marlonn Gillet <gillet.marlonn56@icloud.com>
2026-01-07 12:04:15 +0100

    fonction

---
+++ exercice-fonction/exercice-1/index.js
@@ -0,0 +1,39 @@
+let a = 21;
+let b = 21;
+
+function add(a, b){
+    return a + b;
+}
+console.log (add(a, b));
+
+a = 63;
+b = 21;
+
+function sub (a, b){
+    return a - b;
+}
+console.log(sub(a, b));
+
+a = 21;
+b = 2;
+
+function mult(a, b){
+    return a * b;
+}
+console.log(mult(a, b));
+
+a = 84;
+b = 2;
+
+function div(a, b){
+    return a / b;
+}
+console.log(div(a, b));
+
+a = 42;
+b = 2;
+
+function mod(a, b){
+    return a % b;
+}
+console.log(mod(a, b));
